7482b4b722d3dcc45d97d7f16cddd036df39cff9
ArerepadeB <aredelulu@gmail.com>
2021-12-04 10:00:07 -0600

    mysql db

--- database/variables.tf
+++ database/variables.tf
@@ -2,3 +2,7 @@ variable "rds_pass" {
   type        = string
   description = "MySQL database password"
 }
+variable "instance_type" {
+  type        = string
+  description = "instance size for ec2"
+}
