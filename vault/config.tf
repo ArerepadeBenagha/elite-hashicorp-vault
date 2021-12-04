514fdebc9e26809c9faa24af0c341437aafcf869
ArerepadeB <aredelulu@gmail.com>
2021-12-03 20:44:11 -0600

    intial vault setup

---
+++ vault/config.tf
@@ -0,0 +1,33 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.26.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+  }
+  required_version = ">= 0.14"
+
+  backend "remote" {
+    organization = "EliteSolutionsIT"
+
+    workspaces {
+      name = "elite-gh-actions-test"
+    }
+  }
+}
+
+terraform {
+  required_version = ">=0.12"
+}
+provider "aws" {
+  region = "us-east-1"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
+}
