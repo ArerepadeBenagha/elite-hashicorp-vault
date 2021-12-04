28202d43354b6d65e21eddc21bfa2db2194632c2
ArerepadeB <aredelulu@gmail.com>
2021-12-03 21:06:53 -0600

    intial vault setup

--- vault/config.tf
+++ vault/config.tf
@@ -23,6 +23,7 @@ terraform {
 terraform {
   required_version = ">=0.12"
 }
+
 provider "aws" {
   region = "us-east-1"
 }
---
+++ vault/data.tf
@@ -0,0 +1,26 @@
+data "cloudinit_config" "userdata" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/x-shellscript"
+    filename     = "userdata_docker"
+    content      = templatefile("../templates/userdata_vault.tpl", {})
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
