c023f47060f72cded55eabdc3180a05967310b1e
ArerepadeB <aredelulu@gmail.com>
2021-12-04 11:14:14 -0600

    mysql db

--- database/main.tf
+++ database/main.tf
@@ -141,7 +141,8 @@ resource "aws_db_instance" "mysql" {
   storage_type            = "gp2"
   backup_retention_period = 30                                          # how long you’re going to keep your backups
   availability_zone       = aws_subnet.main-private-1.availability_zone # prefered AZ
-  skip_final_snapshot     = true                                        # skip final snapshot when doing terraform destroy
+  skip_final_snapshot     = true
+  publicly_accessible     = true                                        # skip final snapshot when doing terraform destroy
   tags = {
     Name = "mysql-instance"
   }
--- database/sg.tf
+++ database/sg.tf
@@ -50,13 +50,6 @@ resource "aws_security_group" "main-mysql" {
   }
 
   ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    cidr_blocks = ["70.114.65.185/32"]
-  }
-
-  ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
