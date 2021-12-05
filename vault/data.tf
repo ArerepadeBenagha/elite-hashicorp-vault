b9b7860ef74b897766bb9e5a0735ed0825fbf344
ArerepadeB <aredelulu@gmail.com>
2021-12-05 03:04:23 -0600

    userdata fix

--- vault/data.tf
+++ vault/data.tf
@@ -13,20 +13,30 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+
 #Userdata
 data "cloudinit_config" "userdata" {
   gzip          = true
   base64_encode = true
-  depends_on = [
-    aws_instance.vault-server.id,
-  ]
+
   part {
     content_type = "text/x-shellscript"
     filename     = "userdata_docker"
-    content = templatefile("../templates/userdata_vault.tpl",
-      {
-        localhost = aws_instance.vault-server.public_ip
-      }
-    )
+    content      = templatefile("../templates/userdata_vault.tpl", {})
   }
+  part {
+    content_type = "text/x-shellscript"
+    content      = <<-EOF
+    #!/bin/bash
+    echo 'localhost="${aws_instance.vault-server.public_ip}"' > /opt/server_ip
+    EOF
+  }
+  part {
+    content_type = "text/x-shellscript"
+    content      = data.template_file.client.rendered
+  }
+}
+
+data "template_file" "client" {
+  template = file("../templates/userdata_ip.sh")
 }
