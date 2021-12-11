bfd80a5e1cf78584232e5a4e7b5a40c5ad06d3ed
ArerepadeB <aredelulu@gmail.com>
2021-12-11 02:40:48 -0600

    datadog provider

--- vault/config.tf
+++ vault/config.tf
@@ -27,3 +27,21 @@ terraform {
 provider "aws" {
   region = "us-east-1"
 }
+
+# Terraform 0.13+ uses the Terraform Registry:
+
+terraform {
+  required_providers {
+    datadog = {
+      source = "DataDog/datadog"
+    }
+  }
+}
+
+
+# Configure the Datadog provider
+provider "datadog" {
+  alias   = datadog
+  api_key = var.datadog_api_key
+  app_key = var.datadog_app_key
+}
--- vault/vars.tf
+++ vault/vars.tf
@@ -2,3 +2,9 @@ variable "instance_type" {
   type        = string
   description = "instance size for ec2"
 }
+variable "datadog_api_key" {
+  default = string
+}
+variable "datadog_app_key" {
+  default = string
+}
