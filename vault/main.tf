27cf6956e3ae48d6d2e15c6bc5b51abb63862156
ArerepadeB <aredelulu@gmail.com>
2021-12-12 07:42:17 -0600

    adding www

--- vault/main.tf
+++ vault/main.tf
@@ -325,7 +325,7 @@ resource "aws_route53_record" "www" {
 }
 resource "aws_route53_record" "www-elitedev" {
   zone_id = data.aws_route53_zone.main-zone.zone_id
-  name    = "www"
+  name    = "www.elite-vaultdev.elietesolutionsit.de"
   type    = "CNAME"
   ttl     = "5"
 
