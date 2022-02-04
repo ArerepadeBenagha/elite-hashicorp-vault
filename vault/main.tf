696a3bcd43d295b5d0e215365ffcff8d3a4eb5b5
ArerepadeB <aredelulu@gmail.com>
2022-02-04 09:54:37 -0600

    cert update

--- vault/main.tf
+++ vault/main.tf
@@ -184,18 +184,18 @@ resource "aws_lb_listener" "vault_listA" {
     }
   }
 }
-# ####-------- SSL Cert ------#####
-# resource "aws_lb_listener" "vault_listB" {
-#   load_balancer_arn = aws_lb.vault.arn
-#   port              = "443"
-#   protocol          = "HTTPS"
-#   ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
-#   certificate_arn   = "arn:aws:acm:us-east-1:375866976303:certificate/b5127c10-3d05-41ef-acb4-16973d00cff0"
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.vaultapp_tglb.arn
-#   }
-# }
+####-------- SSL Cert ------#####
+resource "aws_lb_listener" "vault_listB" {
+  load_balancer_arn = aws_lb.vault.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-2-2017-01"
+  certificate_arn   = "arn:aws:acm:us-east-1:375866976303:certificate/6e114ac9-0df7-4812-a049-795698447a35"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.vaultapp_tglb.arn
+  }
+}
 ########------- S3 Bucket -----------####
 resource "aws_s3_bucket" "logs_s3dev" {
   bucket = join("-", [local.application.app_name, "logdev"])
