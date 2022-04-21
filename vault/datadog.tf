6b7570e1e1d28f002df4cf3596cf3a734e6fa5ad
ArerepadeB <aredelulu@gmail.com>
2022-04-20 19:37:35 -0500

    vault

--- vault/datadog.tf
+++ vault/datadog.tf
@@ -1,40 +1,40 @@
-# # Monitor for the the CPU usage.
-# resource "datadog_monitor" "cpu_usage" {
-#   name           = "CPU usage high"
-#   query          = "avg(last_5m):${var.cpu_usage["query"]}{*} by ${var.trigger_by} > ${var.cpu_usage["threshold"]}"
-#   type           = "query alert"
-#   notify_no_data = true
-#   include_tags   = true
+# # # Monitor for the the CPU usage.
+# # resource "datadog_monitor" "cpu_usage" {
+# #   name           = "CPU usage high"
+# #   query          = "avg(last_5m):${var.cpu_usage["query"]}{*} by ${var.trigger_by} > ${var.cpu_usage["threshold"]}"
+# #   type           = "query alert"
+# #   notify_no_data = true
+# #   include_tags   = true
 
-#   message = <<EOM
-# CPU usage high: {{value}}
-# ${var.datadog_alert_footer}
-# EOM
+# #   message = <<EOM
+# # CPU usage high: {{value}}
+# # ${var.datadog_alert_footer}
+# # EOM
+# # }
+
+# # Create a new Datadog monitor
+# resource "datadog_monitor" "elitedatadog" {
+#   name               = "elitedatadog"
+#   type               = "metric alert"
+#   message            = "Monitor triggered. Notify: @hipchat-channel"
+#   escalation_message = "Escalation message @pagerduty"
+#   provider           = datadog.datadog_dev
+
+#   query = "avg(last_5m):${var.cpu_usage["query"]}{*} by ${var.trigger_by} > ${var.cpu_usage["threshold"]}"
+
+#   monitor_thresholds {
+#     warning           = 50
+#     warning_recovery  = 30
+#     critical          = 85
+#     critical_recovery = 50
+#   }
+
+#   notify_no_data    = true
+#   renotify_interval = 60
+
+#   notify_audit = false
+#   timeout_h    = 60
+#   include_tags = true
+
+#   tags = ["Application:vault-server", "datadog"]
 # }
-
-# Create a new Datadog monitor
-resource "datadog_monitor" "elitedatadog" {
-  name               = "elitedatadog"
-  type               = "metric alert"
-  message            = "Monitor triggered. Notify: @hipchat-channel"
-  escalation_message = "Escalation message @pagerduty"
-  provider           = datadog.datadog_dev
-
-  query = "avg(last_5m):${var.cpu_usage["query"]}{*} by ${var.trigger_by} > ${var.cpu_usage["threshold"]}"
-
-  monitor_thresholds {
-    warning           = 50
-    warning_recovery  = 30
-    critical          = 85
-    critical_recovery = 50
-  }
-
-  notify_no_data    = true
-  renotify_interval = 60
-
-  notify_audit = false
-  timeout_h    = 60
-  include_tags = true
-
-  tags = ["Application:vault-server", "datadog"]
-}
--- vault/vars.tf
+++ vault/vars.tf
@@ -2,22 +2,22 @@ variable "instance_type" {
   type        = string
   description = "instance size for ec2"
 }
-variable "datadog_api_key" {
-  type = string
-}
-variable "datadog_app_key" {
-  type = string
-}
+# variable "datadog_api_key" {
+#   type = string
+# }
+# variable "datadog_app_key" {
+#   type = string
+# }
 
 # Variable defining the query and threshold shared by a monitor and graph for the CPU usage.
-variable "cpu_usage" {
-  type = map(string)
+# variable "cpu_usage" {
+#   type = map(string)
 
-  default = {
-    query     = "avg:aws.ec2.cpuutilization"
-    threshold = "85"
-  }
-}
-variable "trigger_by" {
-  default = "{host,env}"
-}
+#   default = {
+#     query     = "avg:aws.ec2.cpuutilization"
+#     threshold = "85"
+#   }
+# }
+# variable "trigger_by" {
+#   default = "{host,env}"
+# }
