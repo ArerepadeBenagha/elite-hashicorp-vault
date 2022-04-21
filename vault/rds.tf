5d7ec10b22f44fc7964b24a1de2e0ab4a0c41c9b
ArerepadeB <aredelulu@gmail.com>
2022-04-21 01:36:57 -0500

    test vault pull secrets

--- vault/rds.tf
+++ vault/rds.tf
@@ -5,8 +5,8 @@ resource "aws_db_instance" "rds" {
   engine_version       = "5.7"
   instance_class       = "db.t3.micro"
   name                 = "mydb"
-  username             = data.vault_generic_secret.secret.path["username"]
-  password             = data.vault_generic_secret.secret.path["password"]
+  username             = data.vault_generic_secret.secret["username"]
+  password             = data.vault_generic_secret.secret["password"]
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
