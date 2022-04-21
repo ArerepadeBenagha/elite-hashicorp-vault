021a268da242ff2b488be7a93406eaa39eb1693c
ArerepadeB <aredelulu@gmail.com>
2022-04-21 18:11:11 -0500

    vault

--- vault/rds.tf
+++ vault/rds.tf
@@ -7,6 +7,6 @@ resource "aws_db_instance" "rds" {
   username             = data.vault_generic_secret.secret.data["username"]
   password             = data.vault_generic_secret.secret.data["password"]
   parameter_group_name = "default.mysql5.7"
-  publicly_accessible  = yes
+  publicly_accessible  = true
   skip_final_snapshot  = true
 }
