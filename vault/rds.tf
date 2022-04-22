1ca6fb339eaed7a86d2539c5a9d775328475637f
ArerepadeB <aredelulu@gmail.com>
2022-04-21 20:19:44 -0500

    vault

--- vault/rds.tf
+++ vault/rds.tf
@@ -1,12 +1,12 @@
-resource "aws_db_instance" "rds" {
-  allocated_storage    = 10
-  engine               = "mysql"
-  engine_version       = "5.7"
-  instance_class       = "db.t3.micro"
-  name                 = "mydb"
-  username             = data.vault_generic_secret.secret.data["username"]
-  password             = data.vault_generic_secret.secret.data["password"]
-  parameter_group_name = "default.mysql5.7"
-  publicly_accessible  = true
-  skip_final_snapshot  = true
-}
+# resource "aws_db_instance" "rds" {
+#   allocated_storage    = 10
+#   engine               = "mysql"
+#   engine_version       = "5.7"
+#   instance_class       = "db.t3.micro"
+#   name                 = "mydb"
+#   username             = data.vault_generic_secret.secret.data["username"]
+#   password             = data.vault_generic_secret.secret.data["password"]
+#   parameter_group_name = "default.mysql5.7"
+#   publicly_accessible  = true
+#   skip_final_snapshot  = true
+# }
